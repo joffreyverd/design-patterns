43866b81b9e815c9cc34b42bcab864e4347c42c3
joffreyverd <jverd@protonmail.com>
2021-01-28 16:26:08 +0100

    fix(inbex.php): fix undefined variable

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once('vendor/autoload.php');
 
 if (sizeof($argv) <= 1 || !in_array($argv[1], App\DesignPattern::LIST)) {
     echo "Please use one of the following pattern as argument: \n";
-    foreach (DESIGN_PATTERNS as $pattern) {
+    foreach (App\DesignPattern::LIST as $pattern) {
         echo "$pattern \n";
     }
     return;
